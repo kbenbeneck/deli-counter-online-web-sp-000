cdbd30ffb427fd4ed2fca98c9435138493655841
Karl Benbeneck <kbenbeneck@gmail.com>
2018-11-04 14:11:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+katz_deli = []
+number = 0
+
+def line(katz_deli)
+  if katz_deli.count == 0
+    puts "The line is currently empty."
+  else
+    line = "The line is currently:"
+    counter = 0
+    until counter == katz_deli.count
+      line << " #{counter +1}. #{katz_deli[counter]}"
+      counter += 1
+    end
+    puts line
+  end
+end
+
+def take_a_number(katz_deli, person)
+  katz_deli << person
+  count = katz_deli.count
+  puts "Welcome, #{person}. You are number #{count} in line."
+end
+
+def now_serving
+end
